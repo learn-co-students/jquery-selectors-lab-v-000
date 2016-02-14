568fa1e0a50fea0481ffd6640ab7282e9a4e24ff
shmulim <shmuel.markel@gmail.com>
2016-02-14 14:20:27 -0800

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 'use-strict';
+
+function h1Selector(){
+  return jQuery('h1');
+}
+
+function liInOlSelector(){
+  return jQuery('ol > li');
+}
+
+function linkSelector(){
+  return jQuery('#box4.box5 > a');
+}
+
+function imageSelector(){
+  return jQuery('img[alt="cat sleeping"]');
+}
+
+function checkboxInputSelector(){
+  return jQuery('input[type="checkbox"]');
+}
