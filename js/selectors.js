7fc0e53689b9c7add99eff8baef8e00fab1a5b5d
John Anthony Rivera <askinganthony@gmail.com>
2016-02-16 13:47:29 -0800

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,7 @@
 'use-strict';
+function h1Selector(){ return $('h1') }
+function liInOlSelector(){return $('ol li')}
+function linkSelector(){return $('#box4.box5 a')}
+function imageSelector(){return $('img[alt="cat sleeping"]')}
+function catSelector(){return $('')}
+function checkboxInputSelector(){return $('input[type="checkbox"]')}
