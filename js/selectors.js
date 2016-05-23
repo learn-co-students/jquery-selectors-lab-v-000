13ececb04a93eb58103aa4999dfb0ced1eccfa9d
agdavid <antonio.g.david@gmail.com>
2016-05-23 13:34:40 -0400

    working js category selectors

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,23 @@
 'use-strict';
+
+function h1Selector() {
+  return $('h1');
+}
+
+function liInOlSelector() {
+  return $('ol li');
+}
+
+function linkSelector() {
+  //both work
+  //return $("div[id='box4'][class='box5'] a");
+  return $('div#box4.box5 a');
+}
+
+function imageSelector() {
+  return $("img[alt='cat sleeping']");
+}
+
+function checkboxInputSelector() {
+  return $(':checkbox');
+}
