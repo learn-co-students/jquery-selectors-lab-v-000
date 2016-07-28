c77814f0355bc4eae713dfd2ae0ff3459fb21fc5
Jason Casares <jasonc@csdrafting.com>
2016-07-28 20:10:55 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,9 +1,22 @@
 'use-strict';
 
 function h1Selector() {
-  return $('.h1');
+  return $('h1');
 }
 
 function liInOlSelector() {
-  return $(ul li);
+  return $('ol li');
+}
+
+function linkSelector() {
+  return $('#box4.box5 a')
+}
+
+function imageSelector() {
+  return $('img[alt="cat sleeping"]')
+
+}
+
+function checkboxInputSelector() {
+  return $('input[type="checkbox"]')
 }
