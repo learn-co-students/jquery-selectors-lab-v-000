111d45d3e9ab9a410627cc5281067625f5cc791e
David Kennell <davidkennell0@gmail.com>
2016-12-26 18:46:21 -0500

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 'use-strict';
+
+function h1Selector() {
+  return $("h1")
+}
+
+function liInOlSelector() {
+  return $("ol li")
+}
+
+function linkSelector() {
+  return $("#box4.box5 a")
+}
+
+function imageSelector() {
+  return $("img[alt='cat sleeping']")
+}
+
+function checkboxInputSelector() {
+  return $(":input:checked")
+}
+
+
+//Write a function, linkSelector, that selects and returns the <a> that is inside of the element that has both an id of box4 and a class of box5.
+//Write a function, imageSelector, that selects and returns the image of the cat that has the alt text equal to "cat sleeping".
+//Write a function, checkboxInputSelector, that selects and returns all the checkbox inputs
