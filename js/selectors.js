0efc0cc7abd27d847ba6439357a2211467bb4b5d
Laurent <lmilne360@gmail.com>
2017-01-09 00:13:49 -0500

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,18 @@
 'use-strict';
+
+function h1Selector() {
+	return $('h1');
+}
+function liInOlSelector() {
+	return $('ol li');
+}
+function linkSelector() {
+	return $('#box4 .box5 a')
+}
+function imageSelector() {
+	return $("img[alt='cat sleeping']") //image with al text " cat sleeping"
+}
+
+function checkboxInputSelector() {
+	return $(":checkbox")
+}
