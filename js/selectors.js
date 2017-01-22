6e2c65210ada96ab352dd0a696d0895b06ad9484
Joe <jsbenning4@gmail.com>
2017-01-21 21:17:46 -0500

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,27 @@
 'use-strict';
+
+function h1Selector() {
+  var allH1 = $('h1');
+  return allH1; 
+}
+
+
+function liInOlSelector() {
+  var allLiOls = $('ol li');
+  return allLiOls;
+}
+
+function linkSelector() {
+  var selectedLinks = $('#box4.box5 a');
+  return selectedLinks;
+}
+
+function imageSelector() {
+  var kitty = $("img[alt='cat sleeping']");
+  return kitty;
+}
+
+function checkboxInputSelector() {
+  var checks = $("input[type='checkbox']");
+  return checks;
+}
