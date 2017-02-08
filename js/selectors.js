0de041b9bbb4ede5dc9f4fd928e6646e5fa37133
ayafushimi <aya.fushimi@gmail.com>
2017-02-08 11:01:31 -0500

    Done

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 'use-strict';
+
+function h1Selector() {
+  $('h1');
+}
+
+function liInOlSelector() {
+  $('ol li');
+}
+
+function linkSelector() {
+  $('.box5#box4 a');
+}
+
+function imageSelector() {
+  $('img[alt="cat sleeping"]');
+}
+
+function checkboxInputSelector() {
+  $('input[type="checkbox"]');
+}
