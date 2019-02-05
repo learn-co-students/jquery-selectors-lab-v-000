d29121f5dc0fc6ec7d0a99cf7e67966ba6ec8470
Christine Cua <cloudycua@gmail.com>
2019-02-05 19:48:33 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 'use-strict';
+
+// all <h1> elements using an element selectro
+function h1Selector(){
+  return $('h1')
+}
+
+// all <li> inside of <ol> using a descendant selector
+function liInOlSelector(){
+  return $('ol li')
+}
+
+// all <a> inside element that has id 'box4' and class 'box5'
+function linkSelector(){
+  return $('#box4.box5 a')
+}
+
+// image of at w/ alt text "cat sleeping"
+function imageSelector(){
+  return $("img[alt='cat sleeping']")
+}
+
+// all checkbox inputs
+function checkboxInputSelector(){
+  return $('input[type="checkbox"]')
+}
